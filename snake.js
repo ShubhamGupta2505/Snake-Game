b3401555baf03704326c0632ea9d3fac3ae7d8df
ShubhamGupta2505 <shubham3nemo@gmail.com>
2022-12-24 16:18:44 +0530

    creating 100 div element using for loop

---
+++ snake.js
@@ -0,0 +1,13 @@
+const grid = document.querySelector('.grid')
+const startButton = document.getElementById('start')
+const score = document.getElementById('score')
+
+function createGrid() {
+    for (let i = 0; i < 100; i++) {
+        const square = document.createElement('div')
+        console.log(square)
+        console.log(i)   
+    }
+}
+
+createGrid()
