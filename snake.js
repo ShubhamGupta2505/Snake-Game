a3398c7b90e92799a8c7f43dc00347dae5e9c116
ShubhamGupta2505 <shubham3nemo@gmail.com>
2022-12-24 16:59:00 +0530

    creating snake

--- snake.js
+++ snake.js
@@ -2,20 +2,19 @@ const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 let squares =[]
-
+let currentsnake = [0,1,2]
 
 function createGrid() {
     for (let i = 0; i < 100; i++) {
         const square = document.createElement('div')
-        console.log(square)
-        console.log(i)
+        
         square.classList.add('square')  
         grid.appendChild(square)
 
         squares.push(square)
         
     }
-    console.log(squares)
+    
 }
 
 createGrid()
