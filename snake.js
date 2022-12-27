248696ba8934d87cba758278e87b05f299b96872
ShubhamGupta2505 <shubham3nemo@gmail.com>
2022-12-27 20:00:57 +0530

    creating boundary restriction for snake

--- snake.js
+++ snake.js
@@ -4,7 +4,7 @@ const score = document.getElementById('score')
 let squares =[]
 let currentsnake = [2,1,0]
 let direction =1
-
+let width = 10
 
 function createGrid() {
     for (let i = 0; i < 100; i++) {
@@ -23,6 +23,14 @@ createGrid()
 
 currentsnake.forEach(index => squares[index].classList.add('snake'))
 function move() {
+    if(
+        (currentsnake[0] + width >= 100 && direction === 10 ) ||
+        (currentsnake[0] % width === 9 && direction === 1) ||
+        (currentsnake[0] % width === 0 && direction === -1) ||
+        (currentsnake[0] - width < 0 && direction === -10) ||
+        squares[currentsnake[0] + direction].classList.contains('snake')
+    )
+    return clearInterval(timerId)
    const tail = currentsnake.pop()
    squares[tail].classList.remove('snake')
    currentsnake.unshift(currentsnake[0] + direction)
@@ -37,15 +45,19 @@ let timerId = setInterval(move,1000)
 function control(e) {
     if(e.keyCode === 39) {
         console.log('right pressed')
+        direction = 1  
     }
     else if(e.keyCode === 37) {
         console.log('left pressed')
+        direction =-1
     }
     else if(e.keyCode === 38) {
         console.log('up pressed')
+        direction = -width
     }
     else if(e.keyCode === 40){
         console.log('down pressed')
+        direction = +width
     }
 }
-// document.addEventListener('keyup',control)
+document.addEventListener('keyup',control)
